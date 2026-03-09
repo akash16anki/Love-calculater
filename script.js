b5d621a50d78966ac08bf7643c4decab70ba30e4
Akash solanki <akash16anki14@gmail.com>
2026-03-09 07:16:42 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+// input boy name
+
+const form = document.querySelector('form');
+
+form.addEventListener('submit',(e)=>{
+     e.preventDefault();
+    const boy = document.getElementById("yourName");
+    const girl = document.getElementById("CrushName");
+    const result = document.getElementById("actualR");
+    const l1 = (boy.value.length);
+        const l2 = (girl.value.length);
+        const calculate =(l1*l2)+20;
+    result.textContent=`${calculate}%`;
+    form.reset();
+
+});
